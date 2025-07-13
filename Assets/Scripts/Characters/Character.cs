f3376e9d32000f5eedf8598a270d3350b824f7fd
liyard-tls <mckinley55X@gmail.com>
2025-07-13 22:20:49 +0300

    DOtween & fast realod & minor fixes

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using Data;
+using DG.Tweening;
 using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Events;
@@ -13,6 +15,8 @@ namespace Characters
         [Header("Visual")]
         [JsonIgnore]
         public SpriteRenderer SpriteRenderer;
+        public Vector2 BreatheScaleOffset = new Vector2(0.025f, 0.05f);
+        public float BreatheSpeed = 1f;
     
         [Header("State")]
         public CharacterState currentState;
@@ -28,12 +32,27 @@ namespace Characters
         protected virtual void InitializeCharacter()
         {
             currentState = CharacterState.Idle;
+            Invoke(nameof(IdleBreatheAnimation), Random.Range(0f, BreatheSpeed));
         }
     
         public virtual void ChangeState(CharacterState newState)
         {
             currentState = newState;
             OnStateChanged?.Invoke(this);
+        }
+        
+        private void IdleBreatheAnimation()
+        {
+            // DOTween sprite scale animation
+            Vector2 startScale = SpriteRenderer.transform.localScale;
+            Vector2 endScale = startScale + BreatheScaleOffset;
+            
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(SpriteRenderer.transform.DOScale(endScale, BreatheSpeed)
+                .SetEase(Ease.InOutSine))
+                .Append(SpriteRenderer.transform.DOScale(startScale, BreatheSpeed)
+                .SetEase(Ease.InOutSine))
+                .OnComplete(IdleBreatheAnimation);
         }
     }
 
